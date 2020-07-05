adba82332c42afe026ec95efeb35defd9467a673
Suresh Chaudhari <codebrust@gmail.com>
2020-07-05 16:25:32 +0545

    Initial Commit

---
+++ XCodeStarterProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCodeStarterProject
+//
+//  Created by Suresh Chaudhari on 7/5/20.
+//  Copyright © 2020 codebrust. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
